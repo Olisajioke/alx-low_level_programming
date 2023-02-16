7f40ffcbff2cec46e9f3c75c115cbe11a9b77b7f
Olisajioke <chijioke914@gmail.com>
2023-02-16 07:56:43 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-/*
- * main - print a line of ode using puts
+/**
+ * main-print a line of code using puts
  *
  * Return: 0
 */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
-~
-~
 ~
 ~
 ~
