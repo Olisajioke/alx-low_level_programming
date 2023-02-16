fe2e25e7d2a42af8483663c3779cdda28457d752
Olisajioke <chijioke914@gmail.com>
2023-02-16 11:53:45 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,12 @@
  * Return: 0
 */
 int main(void)
-{
+{	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
 	printf("Size of char: %lu byte(s)\n", sizeof(char));
 	printf("Size of int: %lu byte(s)\n", sizeof(int));
 	printf("Size of long: %lu byte(s)\n", sizeof(long));
