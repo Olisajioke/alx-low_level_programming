36c8ef91d76621a53c06b25176987921b2055be0
Olisajioke <chijioke914@gmail.com>
2023-02-16 09:47:57 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,16 @@
 */
 int main(void)
 {
-	printf("Size of char: %lu byte\n", sizeof(char));
-	printf("Size of short: %lu bytes\n", sizeof(short));
-	printf("Size of int: %lu bytes\n", sizeof(int));
-	printf("Size of long: %lu bytes\n", sizeof(long));
-	printf("Size of long long: %lu bytes\n", sizeof(long long));
-	printf("Size of float: %lu bytes\n", sizeof(float));
-	printf("Size of double: %lu bytes\n", sizeof(double));
-	printf("Size of long double: %lu bytes\n", sizeof(long double));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of short: %lu byte(s)\n", sizeof(short));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long: %lu byte(s)\n", sizeof(long));
+	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of double: %lu byte(s)\n", sizeof(double));
+	printf("Size of long double: %lu byte(s)\n", sizeof(long double));
+	printf("Size of uintptr_t: %lu byte(s)\n", sizeof(uintptr_t));
+	printf("Size of size_t: %lu byte(s)\n", sizeof(size_t));
+	printf("Size of void*: %lu byte(s)\n", sizeof(void));
 	return (0);
 }
