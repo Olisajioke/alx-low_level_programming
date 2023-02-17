34273053719c03f4cd616f8f4c62585c9f5a4ce5
Olisajioke <chijioke914@gmail.com>
2023-02-17 15:58:53 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - main code
+ *code by Olisajioke
+ *Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
