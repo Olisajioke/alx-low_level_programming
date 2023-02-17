f9c2959c01e9b63eadf288968d24f32b61e46070
Olisajioke <chijioke914@gmail.com>
2023-02-17 15:22:31 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - main block
+ *code by Olisajioke
+ *Description; print the alphabet in lowercase
+ *Return: 0
+*/
+int main(void)
+{
+
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+
+	putchar('\n');
+	return (0);
+}
