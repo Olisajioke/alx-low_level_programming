2ab5263fd1dbcedb4834090c62f9d01b83d74153
Olisajioke <chijioke914@gmail.com>
2023-02-17 14:05:24 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *code by Olisajioke
+ *Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	c = '#';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
