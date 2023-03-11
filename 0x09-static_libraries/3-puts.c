ad8ced7df05f34e387c66a9ba731323c0473c02c
Olisajioke <chijioke914@gmail.com>
2023-03-11 11:10:50 -0800

    Task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+
+void _puts(char *str)
+
+/**
+* main -Entry point
+*_puts -Two strings
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
