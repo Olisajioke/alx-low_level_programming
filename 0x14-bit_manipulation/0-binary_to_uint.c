0d67b1a05d936477a12158f69483b6156f4b73c1
Olisajioke <chijioke914@gmail.com>
2023-04-07 08:25:09 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int x;
-	unsigned int dec_val = 0;
+int z;
+unsigned int dec_val = 0;
 
-	if (!b)
-		return (0);
+if (b == NULL)
+return (0);
 
-	for (int x = 0; b[x]; x++)
-	{
-		if (b[x] < '0' || b[x] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[x] - '0');
-	}
+for (z = 0; b[z] != '\0'; z++)
+{
+if (b[z] != '0' && b[z] != '1')
+return (0);
+dec_val = (dec_val << 1) + (b[z] - '0');
+}
 
-	return (dec_val);
+return (dec_val);
 }
