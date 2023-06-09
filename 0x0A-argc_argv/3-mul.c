54efd5cd793600bfcd66142088e17aef76839ac8
Olisajioke <chijioke914@gmail.com>
2023-06-09 14:07:26 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,36 +9,39 @@
 
 int convert_to_int(char *s)
 {
-int n = 0;
-int digit;
-int len = 0;
-int sign = 0;
+int x, z, a, index, f, integer;
 
-for (len = 0; s[len] != '\0'; len++)
+x = 0;
+z = 0;
+a = 0;
+index = 0;
+f = 0;
+integer = 0;
+
+for (index = 0; s[index] != '\0'; index++)
 ;
 
-for (int i = 0; i < len && sign == 0; i++)
+for (x = 0; x < index && f == 0; x++)
 {
-if (s[i] == '-')
-sign++;
-
-if (s[i] >= '0' && s[i] <= '9')
+if (s[x] == '-')
+++z;
+if (s[x] >= '0' && s[x] <= '9')
 {
-	digit = s[i] - '0';
-	if (sign % 2)
-		digit = -digit;
-	n = n * 10 + digit;
-	sign = 1;
-	if (s[i + 1] < '0' || s[i + 1] > '9')
-	break;
-	sign = 0;
+	integer = s[x] - '0';
+	if (z % 2)
+		integer = -integer;
+	a = a * 10 + integer;
+	f = 1;
+	if (s[x + 1] < '0' || s[x + 1] > '9')
+		break;
+	f = 0;
+}
 }
 
-}
-if (sign == 0)
+if (f == 0)
 return (0);
 
-return (n);
+return (a);
 }
 
 /**
@@ -47,12 +50,12 @@ return (n);
 *@argv: array of arguments
 *Return: 0 (Success), 1 (Error)
 */
+
 int main(int argc, char *argv[])
 {
-
 int result, num1, num2;
 
-if (argc != 3)
+if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
@@ -61,6 +64,8 @@ return (1);
 num1 = convert_to_int(argv[1]);
 num2 = convert_to_int(argv[2]);
 result = num1 *num2;
+
 printf("%d\n", result);
+
 return (0);
 }
