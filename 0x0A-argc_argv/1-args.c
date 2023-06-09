67d1fa732197a93c5ab5c5367c369cd4e975d398
Olisajioke <chijioke914@gmail.com>
2023-06-09 11:33:26 -0700

    Task 01

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,16 +2,37 @@
 #include "main.h"
 
 /**
-*main - Prints the number of arguments passed to the program.
-*@argc: Number of arguments.
-*@argv: Array of arguments.
-*Return: Always 0 (Success).
-*/
-
-int main(int argc, char argv[])
+ * main - Prints the number of arguments passed to the program.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char *argv[])
 {
-(void)argv;
-printf("%d\n", argc - 1);
+	/* Ignore unused variable */
+	(void)argv;
 
-return (0);
+	/* Calculate the number of arguments */
+	int num_arguments = argc - 1;
+
+	/* Print the number of arguments character by character */
+	if (num_arguments == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		while (num_arguments != 0)
+		{
+			/* Convert the digit to character and print */
+			_putchar(num_arguments % 10 + '0');
+			num_arguments /= 10;
+		}
+	}
+
+	/* Print new line character */
+	_putchar('\n');
+
+	return (0);
 }
