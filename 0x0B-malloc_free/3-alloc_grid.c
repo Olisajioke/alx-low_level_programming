943f670719d66802225822a37c859380398f0605
Olisajioke <chijioke914@gmail.com>
2023-06-13 14:01:48 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,51 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * alloc_grid - nested loop to make grid
+ * alloc_grid - allocate a 2D grid of integers
  * @width: width input
  * @height: height input
- * Return: pointer to 2 dim. array
+ * Return: pointer to 2D array
  */
-
 int **alloc_grid(int width, int height)
 {
-int **lens;
-int a, b;
+	int **grid;
+	int a, b;
 
-if (width <= 0 || height <= 0)
-return (NULL);
-lens = malloc(sizeof(int *) * height);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-if (lens == NULL)
-return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
 
-a = 0;
-while (a < height)
-{
-lens[a] = malloc(sizeof(int) * width);
-}
-if (lens[a] == NULL
-{
-while (a >= 0)
-{
-free(lens[a]);
-a--;
-}
-free(lens);
-return (NULL);
-}
-a++;
-}
-a = 0;
-while (a < height)
-{
-b = 0;
-while (b < width)
-{
-lens[a][b] = 0;
-b++;
-}
-a++;
-}
-return (lens);
+	a = 0;
+	while (a < height)
+	{
+		grid[a] = malloc(sizeof(int) * width);
+		if (grid[a] == NULL)
+		{
+			while (a >= 0)
+			{
+				free(grid[a]);
+				a--;
+			}
+			free(grid);
+			return (NULL);
+		}
+		a++;
+	}
+
+	a = 0;
+	while (a < height)
+	{
+		b = 0;
+		while (b < width)
+		{
+			grid[a][b] = 0;
+			b++;
+		}
+		a++;
+	}
+
+	return (grid);
 }
